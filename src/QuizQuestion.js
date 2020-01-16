d89ca521effa66698124500f324d69e801371483
Magno Nascimento <magno@magnonascimento.com>
2020-01-15 22:28:44 -0300

    Adicionado condicional ao componente quiz.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,16 +4,18 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component {
 
   render() {
-    return  <main>
-      <section>
-        <p>{this.props.quiz_question.instruction_text}</p>
-      </section>
-      <section className="buttons">
-        <ul>
-          <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
-        </ul>
-      </section>
-    </main>
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+          </ul>
+        </section>
+      </main>
+    )
   }
   
 }
